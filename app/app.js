a1a352b269d4e277c1e7b191af56f9d0a63764a4
Soham Pramanik <clashersoham07@gmail.com>
2026-05-20 23:43:20 +0530

    spaceStation small bug fixes

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import t from "@titanpl/route";
 
 t.ws("/chat").action("chat");
 t.get("/").action("ui")
-t.get("/image").action("stream")
+
 t.get("/test").reply("huihuihui!")
+
 t.start(5100, "Titan Running!");
